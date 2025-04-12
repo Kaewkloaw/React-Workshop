9a2c05a7c6142aa77b7347ce65922b513ab6869f
Punchaya Chancharoen <punchaya.chan@gmail.com>
2025-04-13 01:13:55 +0700

    update file

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -2,15 +2,17 @@ import {createContext, useState} from 'react'
 import Menu from './components/Menu'
 import Quiz from './components/Quiz'
 import Score from './components/Score'
-
+import './App.css'
 export const DataContext = createContext()
 
 function App() {
-  const [appState, setAppState] = useState("menu");
+  const [appState, setAppState] = useState("menu")
+  const [score, setScore] = useState(0)
+
   return (
-    <DataContext.Provider value={{appState, setAppState}}>
+    <DataContext.Provider value={{appState, setAppState , score, setScore}}>
       <div className="App">
-        <h1>Web Development Quiz</h1>
+        <h1 className = 'Header'>Web Development Quiz</h1>
         {appState === "menu" && <Menu />}
         {appState === "quiz" && <Quiz />}
         {appState === "score" && <Score />}
--- quiz-app/src/components/Quiz.js
+++ quiz-app/src/components/Quiz.js
@@ -1,14 +1,51 @@
-import { DataContext } from "../App"
-import { useContext } from "react"
+import { DataContext } from "../App";
+import { useContext, useState, useEffect } from "react";
+import QuestionsData from "../data/QuestionsData";
 
 const Quiz = () => {
-    const {setAppState} = useContext(DataContext)
+    const {setAppState, score, setScore } = useContext(DataContext);
+    const [current, setCurrent] = useState(0);
+    const [selectChoice, setSelectChoice] = useState("");
+
+    useEffect(() => {
+        CheckAnswer();
+        // eslint-disable-next-line
+    }, [selectChoice]);
+
+    const CheckAnswer = () => {
+        if (selectChoice !== "") {
+            if (selectChoice === QuestionsData[current].answer) {
+                setScore(score + 1);
+                nextQuestion();
+            } else {
+                console.log("Wrong Answer");
+                nextQuestion();
+            }
+        }
+    };
+
+    const nextQuestion = () => {
+        setSelectChoice("");
+        if (current === QuestionsData.length - 1) { // 0 to 4 = 5 questions
+            setAppState("score");
+        }
+         else if (current < QuestionsData.length - 1) {
+            setCurrent(current + 1);
+        }
+    };
+
     return (
         <div className="quiz">
-            <h1>Quiz Component</h1>
-            <button onClick={()=>setAppState("menu")}>Back</button>
+            <p>{current + 1} / {QuestionsData.length}</p>
+            <h1>{QuestionsData[current].question}</h1>
+            <div className="options">
+                <button onClick={() => setSelectChoice("A")}>{QuestionsData[current].A}</button>
+                <button onClick={() => setSelectChoice("B")}>{QuestionsData[current].B}</button>
+                <button onClick={() => setSelectChoice("C")}>{QuestionsData[current].C}</button>
+                <button onClick={() => setSelectChoice("D")}>{QuestionsData[current].D}</button>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Quiz;
